bd7cf6f9e87b674da434a964c0b54a28c78d5496
Pakjira Jitsawatpaiboon <jitdr116@student.otago.ac.nz>
2021-01-22 20:56:18 +1300

    Fixing collision

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,11 +9,37 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
     Vector2 movement;
 
+    void Awake(){
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void HandleMovement(){
+        float moveX = 0f;
+        float moveY = 0f;
+
+        if(Input.GetKey(KeyCode.W)){
+            moveY = +1f;
+        }
+        if(Input.GetKey(KeyCode.S)){
+            moveY = -1f;
+        }
+        if(Input.GetKey(KeyCode.A)){
+            moveX = -1f;
+        }
+        if(Input.GetKey(KeyCode.D)){
+            moveX = +1f;
+        }
+        
+        Vector3 moveDir = new Vector3(moveX,moveY).normalized;
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+    }
+
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        //HandleMovement();
     }
 
     void FixedUpdate(){
@@ -21,6 +47,24 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void onTriggerEnter2D(Collider2D collider){
-        Debug.Log("Trigger");
+        Debug.Log("TriggerEnter");
+    }    
+
+    void onCollisionEnter2D(Collision2D collision){
+        Debug.Log("CollisionEnter");
+    } 
+    void onTriggerExit2D(Collider2D collider){
+        Debug.Log("TriggerExit");
+    }    
+
+    void onCollisionExit2D(Collision2D collision){
+        Debug.Log("CollisionExit");
+    } 
+    void onTriggerStay2D(Collider2D collider){
+        Debug.Log("TriggerStay");
+    }    
+
+    void onCollisionStay2D(Collision2D collision){
+        Debug.Log("CollisionStay");
     } 
 }
