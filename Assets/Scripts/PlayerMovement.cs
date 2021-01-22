e0c438e1562097848be1e7b1540e5a34099d7151
Pakjira Jitsawatpaiboon <jitdr116@student.otago.ac.nz>
2021-01-22 21:12:34 +1300

    Fixed collision not reigistered

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,63 +8,39 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
     Vector2 movement;
-
-    void Awake(){
-        rb = gameObject.GetComponent<Rigidbody2D>();
-    }
-
-    void HandleMovement(){
-        float moveX = 0f;
-        float moveY = 0f;
-
-        if(Input.GetKey(KeyCode.W)){
-            moveY = +1f;
-        }
-        if(Input.GetKey(KeyCode.S)){
-            moveY = -1f;
-        }
-        if(Input.GetKey(KeyCode.A)){
-            moveX = -1f;
-        }
-        if(Input.GetKey(KeyCode.D)){
-            moveX = +1f;
-        }
-        
-        Vector3 moveDir = new Vector3(moveX,moveY).normalized;
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-        //HandleMovement();
     }
 
     void FixedUpdate(){
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 
-    void onTriggerEnter2D(Collider2D collider){
+    //Collision Trigger
+    void OnTriggerEnter2D(Collider2D collider){
+        //add code here
+        
         Debug.Log("TriggerEnter");
+    }  
+    void OnTriggerStay2D(Collider2D collider){
+        //Debug.Log("TriggerStay");
     }    
-
-    void onCollisionEnter2D(Collision2D collision){
-        Debug.Log("CollisionEnter");
-    } 
-    void onTriggerExit2D(Collider2D collider){
+    void OnTriggerExit2D(Collider2D collider){
         Debug.Log("TriggerExit");
     }    
 
-    void onCollisionExit2D(Collision2D collision){
-        Debug.Log("CollisionExit");
-    } 
-    void onTriggerStay2D(Collider2D collider){
-        Debug.Log("TriggerStay");
-    }    
 
-    void onCollisionStay2D(Collision2D collision){
+    void OnCollisionEnter2D(Collision2D collision){
+        Debug.Log("CollisionEnter");
+    }     
+    void OnCollisionExit2D(Collision2D collision){
+        Debug.Log("CollisionExit");
+    }      
+    void OnCollisionStay2D(Collision2D collision){
         Debug.Log("CollisionStay");
     } 
 }
